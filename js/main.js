7bf4ee79cc73e307d4efb0cab7e8e89ad12ff079
Alexander Shmidt <shmidt.alex@gmail.com>
2019-02-25 20:09:57 +0300

    code refactored according to new data structure

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const contentLilbraries = ['React', 'jQuery'];
-const contentFrameworks = ['Angular', 'Vue'];
-const sidebarContent = ['Libraries', 'Frameworks'];
+const items = [
+    { name: 'Libraries', items: [{ name: 'React' }, { name: 'jQuery' }, ], },
+    { name: 'Frameworks', items: [{ name: 'Angular' }, { name: 'Vue' }, ], }
+]
 
 const containerBlock = document.querySelector('.container');
 
@@ -16,35 +17,47 @@ containerBlock.appendChild(rightSidebar);
 containerBlock.appendChild(mainSection);
 rightSidebar.appendChild(mainList);
 rightSidebar.setAttribute('class', 'rightSidebar');
-mainSection.setAttribute('class', 'mainSection')
+mainSection.setAttribute('class', 'mainSection');
 
-function creatingElements(parrentNode, contentArr, elementName, elementClass) {
-    contentArr.forEach(function(item, index) {
-        item = document.createElement(elementName);
-        item.setAttribute('class', elementClass);
-        item.setAttribute('id', contentArr[index]);
-        parrentNode.appendChild(item);
-        let text = document.createTextNode(contentArr[index]);
-        item.appendChild(text);
-    })
+function bulidMenu(element, items, tagName) {
+    let newElement;
+    Object.keys(items).forEach(function(key) {
+        let item = items[key];
+        Object.keys(item).forEach(function(key) {
+            let subItem;
+            if (!Array.isArray(item[key])) {
+                subItem = item[key];
+                newElement = document.createElement(tagName);
+                element.appendChild(newElement);
+                newElement.setAttribute('class', `${subItem} listComponents`);
+                let text = document.createTextNode(subItem);
+                newElement.appendChild(text);
+            } else {
+                bulidMenu(newElement, item[key], 'li');
+                if (newElement.children) {
+                    newElement.classList.add('dropDownList');
+                    newElement.classList.remove('listComponents');
+                }
+            }
+
+        })
+    });
+
 }
-creatingElements(mainList, sidebarContent, 'li', 'drop-down-list');
-const libraries = document.getElementById('Libraries');
-const frameworks = document.getElementById('Frameworks');
-creatingElements(libraries, contentLilbraries, 'li', 'libraries list-components');
-creatingElements(frameworks, contentFrameworks, 'li', 'frameworks list-components');
+bulidMenu(mainList, items, 'li');
 
 function expandList(e) {
-	let element = e.target.children;
-	console.log(element.length);
-	for (let i = 0; i < element.length; i++){
-		if (element[i].style.display !== 'none'){
-			element[i].style.display = 'none';
-		} else {
-			element[i].style.display = 'inherit';
-		}
-	}	
+    let element = e.target.children;
+    for (let i = 0; i < element.length; i++) {
+        if (element[i].style.display !== 'none') {
+            element[i].style.display = 'none';
+        } else {
+            element[i].style.display = 'inherit';
+        }
+    }
 }
 
+const libraries = document.querySelector('.Libraries');
+const frameworks = document.querySelector('.Frameworks');
 libraries.addEventListener('click', expandList);
 frameworks.addEventListener('click', expandList);
